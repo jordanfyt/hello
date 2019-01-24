fab2f8ae55f6d8660cc54b6b9f1ed0f313ad10ed
Iordanis Fytanoglou <iordanis.fytanoglou@kpmg.co.uk>
2019-01-24 13:59:29 +0000

    first test with jenkins

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessage());
+        }
+
+        public static string CreateMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ HelloWorldTests/Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using HelloWorld;
+
+namespace HelloWorldTests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+            Assert.AreEqual("Hello World", MainClass.CreateMessage());
+        }
+    }
+}
